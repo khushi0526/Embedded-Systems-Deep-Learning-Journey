c747a4620a8a768b32e26d7f9585fbb3347ba6d6
KHUSHI B S <bskhushi2@gmail.com>
2026-05-05 23:58:52 +0530

    Update code.ino

--- 29/code.ino
+++ 29/code.ino
@@ -11,18 +11,13 @@ void loop() {
 
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-
   long duration = pulseIn(echoPin, HIGH);
-
   float distance = duration * 0.034 / 2;
-
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
-
   delay(500);
 }
